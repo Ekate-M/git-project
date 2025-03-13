d6c15c4fbb1ea3197b548cf08e76b5ac5ffd8eba
Katerina <ekate-meshkova@yandex.ru>
2025-03-13 13:54:51 +0300

    first commit

---
+++ untitled/src/BinarySearch.java
@@ -0,0 +1,17 @@
+public class BinarySearch {
+public static <T> int rank  (T key, T [ ] a, Comparator<T> comparator) {
+        int min = 0;
+        int max = a.length - 1;
+        while (min <= max) {
+        int mid = ((max - min) / 2) + min;
+        if (comparator.compare(key, a [mid]) > 0)
+        min = mid + 1;
+        else if (comparator.compare (key, a[mid]) < 0)
+        max = mid - 1;
+        else if (comparator.compare (key, a[mid] )==0) {
+        return mid;
+        }
+        }
+        return -1;
+        }
+        }
